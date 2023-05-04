07cc065b4bfd0486cde022dff6ed6c4af5a966e3
Senull0 <fnull@tuta.io>
2023-05-04 22:56:07 +0300

    Initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import {UserContextProvider} from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = 'https://two34-bndd.onrender.com';
+  axios.defaults.baseURL = 'http://localhost:4040';
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
